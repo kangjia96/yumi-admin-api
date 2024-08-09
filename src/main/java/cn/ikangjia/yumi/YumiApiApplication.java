c1c39c744ef69079015327abba912b22b4cf9882
kangjia <ikangjia.cn@outlook.com>
2024-08-09 21:34:51 +0800

    🎉init...

---
+++ src/main/java/cn/ikangjia/yumi/YumiApiApplication.java
@@ -0,0 +1,13 @@
+package cn.ikangjia.yumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumiApiApplication.class, args);
+    }
+
+}
